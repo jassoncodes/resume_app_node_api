1c17d7a8e96b7415c140642414a2d260ad9ac3fc
Jasson <jpincay93@outlook.com>
2025-02-19 01:54:57 -0500

    fixed server.cjs for cpanel deploy

--- src/server.cjs
+++ src/server.cjs
@@ -1,3 +1,3 @@
-const app = require("./dist/index.js"); // O donde esté tu app en dist
+const app = require("./dist/server.js"); // O donde esté tu app en dist
 
 app.listen(3000, () => console.log("Server running"));
