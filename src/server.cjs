dce820e819da71dd3f24031c3fc421e10f2b7c68
Jasson <jpincay93@outlook.com>
2025-02-19 01:50:11 -0500

    fixed server.cjs for cpanel deploy

--- src/server.cjs
+++ src/server.cjs
@@ -1,6 +1,3 @@
-const { createRequire } = require("module");
-const require = createRequire(import.meta.url);
-
 const app = require("./dist/index.js"); // O donde esté tu app en dist
 
 app.listen(3000, () => console.log("Server running"));
